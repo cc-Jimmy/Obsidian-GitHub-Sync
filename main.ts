47e3e93cce4ee3e1d8eec5958d28b1b1411c54ff
Jimmy <xx@xx.com>
2025-02-15 15:52:48 +0800

    🌈 style: format

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Vault } from 'obsidian';
-import { simpleGit, SimpleGit, CleanOptions, SimpleGitOptions } from 'simple-git';
-import { setIntervalAsync, clearIntervalAsync } from 'set-interval-async';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	Vault,
+} from "obsidian";
+import {
+	simpleGit,
+	SimpleGit,
+	CleanOptions,
+	SimpleGitOptions,
+} from "simple-git";
+import { setIntervalAsync, clearIntervalAsync } from "set-interval-async";
 
 let simpleGitOptions: Partial<SimpleGitOptions>;
 let git: SimpleGit;
-
 
 interface GHSyncSettings {
 	remoteURL: string;
@@ -14,28 +28,26 @@ interface GHSyncSettings {
 }
 
 const DEFAULT_SETTINGS: GHSyncSettings = {
-	remoteURL: '',
-	gitLocation: '',
+	remoteURL: "",
+	gitLocation: "",
 	syncinterval: 0,
 	isSyncOnLoad: false,
-}
-
+};
 
 export default class GHSyncPlugin extends Plugin {
 	settings: GHSyncSettings;
 
-	async SyncNotes()
-	{
-		new Notice("Syncing to GitHub remote")
+	async SyncNotes() {
+		new Notice("Syncing to GitHub remote");
 
 		const remote = this.settings.remoteURL;
 
 		simpleGitOptions = {
 			//@ts-ignore
-		    baseDir: this.app.vault.adapter.getBasePath(),
-		    binary: this.settings.gitLocation + "git",
-		    maxConcurrentProcesses: 6,
-		    trimmed: false,
+			baseDir: this.app.vault.adapter.getBasePath(),
+			binary: this.settings.gitLocation + "git",
+			maxConcurrentProcesses: 6,
+			trimmed: false,
 		};
 		git = simpleGit(simpleGitOptions);
 
@@ -43,36 +55,54 @@ export default class GHSyncPlugin extends Plugin {
 		let hostname = os.hostname();
 
 		let statusResult = await git.status().catch((e) => {
-			new Notice("Vault is not a Git repo or git binary cannot be found.", 10000);
-			return; })
+			new Notice(
+				"Vault is not a Git repo or git binary cannot be found.",
+				10000
+			);
+			return;
+		});
 
 		//@ts-ignore
 		let clean = statusResult.isClean();
 
-    	let date = new Date();
-    	let msg = hostname + " " + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		let date = new Date();
+		let msg =
+			hostname +
+			" " +
+			date.getFullYear() +
+			"-" +
+			(date.getMonth() + 1) +
+			"-" +
+			date.getDate() +
+			":" +
+			date.getHours() +
+			":" +
+			date.getMinutes() +
+			":" +
+			date.getSeconds();
 
 		// git add .
 		// git commit -m hostname-date-time
 		if (!clean) {
 			try {
-				await git
-		    		.add("./*")
-		    		.commit(msg);
-		    } catch (e) {
-		    	new Notice(e);
-		    	return;
-		    }
+				await git.add("./*").commit(msg);
+			} catch (e) {
+				new Notice(e);
+				return;
+			}
 		} else {
 			new Notice("Working branch clean");
 		}
 
 		// configure remote
 		try {
-			await git.removeRemote('origin').catch((e) => { new Notice(e); });
-			await git.addRemote('origin', remote).catch((e) => { new Notice(e); });
-		}
-		catch (e) {
+			await git.removeRemote("origin").catch((e) => {
+				new Notice(e);
+			});
+			await git.addRemote("origin", remote).catch((e) => {
+				new Notice(e);
+			});
+		} catch (e) {
 			new Notice(e);
 			return;
 		}
@@ -86,79 +116,88 @@ export default class GHSyncPlugin extends Plugin {
 
 		new Notice("GitHub Sync: Successfully set remote origin url");
 
-
 		// git pull origin main
-	    try {
-	    	//@ts-ignore
-	    	await git.pull('origin', 'main', { '--no-rebase': null }, (err, update) => {
-	      		if (update) {
-					new Notice("GitHub Sync: Pulled " + update.summary.changes + " changes");
-	      		}
-	   		})
-	    } catch (e) {
-	    	let conflictStatus = await git.status().catch((e) => { new Notice(e, 10000); return; });
-    		let conflictMsg = "Merge conflicts in:";
-	    	//@ts-ignore
-			for (let c of conflictStatus.conflicted)
-			{
-				conflictMsg += "\n\t"+c;
+		try {
+			//@ts-ignore
+			await git.pull(
+				"origin",
+				"main",
+				{ "--no-rebase": null },
+				(err, update) => {
+					if (update) {
+						new Notice(
+							"GitHub Sync: Pulled " +
+								update.summary.changes +
+								" changes"
+						);
+					}
+				}
+			);
+		} catch (e) {
+			let conflictStatus = await git.status().catch((e) => {
+				new Notice(e, 10000);
+				return;
+			});
+			let conflictMsg = "Merge conflicts in:";
+			//@ts-ignore
+			for (let c of conflictStatus.conflicted) {
+				conflictMsg += "\n\t" + c;
 			}
-			conflictMsg += "\nResolve them or click sync button again to push with unresolved conflicts."
-			new Notice(conflictMsg)
-			//@ts-ignore	
-			for (let c of conflictStatus.conflicted)
-			{
+			conflictMsg +=
+				"\nResolve them or click sync button again to push with unresolved conflicts.";
+			new Notice(conflictMsg);
+			//@ts-ignore
+			for (let c of conflictStatus.conflicted) {
 				this.app.workspace.openLinkText("", c, true);
 			}
-	    	return;
-	    }
+			return;
+		}
 
 		// resolve merge conflicts
 		// git push origin main
-	    if (!clean) {
-		    try {
-		    	git.push('origin', 'main', ['-u']);
-		    	new Notice("GitHub Sync: Pushed on " + msg);
-		    } catch (e) {
-		    	new Notice(e, 10000);
+		if (!clean) {
+			try {
+				git.push("origin", "main", ["-u"]);
+				new Notice("GitHub Sync: Pushed on " + msg);
+			} catch (e) {
+				new Notice(e, 10000);
 			}
-	    }
+		}
 	}
-
 
 	async onload() {
 		await this.loadSettings();
 
-		const ribbonIconEl = this.addRibbonIcon('github', 'Sync with Remote', (evt: MouseEvent) => {
-			this.SyncNotes();
-		});
-		ribbonIconEl.addClass('gh-sync-ribbon');
-
-		this.addCommand({
-			id: 'github-sync-command',
-			name: 'Sync with Remote',
-			callback: () => {
+		const ribbonIconEl = this.addRibbonIcon(
+			"github",
+			"Sync with Remote",
+			(evt: MouseEvent) => {
 				this.SyncNotes();
 			}
+		);
+		ribbonIconEl.addClass("gh-sync-ribbon");
+
+		this.addCommand({
+			id: "github-sync-command",
+			name: "Sync with Remote",
+			callback: () => {
+				this.SyncNotes();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new GHSyncSettingTab(this.app, this));
 
-		if (!isNaN(this.settings.syncinterval))
-		{
+		if (!isNaN(this.settings.syncinterval)) {
 			let interval: number = this.settings.syncinterval;
-			if (interval >= 1)
-			{
+			if (interval >= 1) {
 				try {
 					setIntervalAsync(async () => {
 						await this.SyncNotes();
 					}, interval * 60 * 1000);
 					//this.registerInterval(setInterval(this.SyncNotes, interval * 6 * 1000));
 					new Notice("Auto sync enabled");
-				} catch (e) {
-					
-				}
+				} catch (e) {}
 			}
 		}
 
@@ -166,32 +205,30 @@ export default class GHSyncPlugin extends Plugin {
 		try {
 			simpleGitOptions = {
 				//@ts-ignore
-			    baseDir: this.app.vault.adapter.getBasePath(),
-			    binary: this.settings.gitLocation + "git",
-			    maxConcurrentProcesses: 6,
-			    trimmed: false,
+				baseDir: this.app.vault.adapter.getBasePath(),
+				binary: this.settings.gitLocation + "git",
+				maxConcurrentProcesses: 6,
+				trimmed: false,
 			};
 			git = simpleGit(simpleGitOptions);
 
 			//check for remote changes
 			// git branch --set-upstream-to=origin/main main
-			await git.branch({'--set-upstream-to': 'origin/main'});
+			await git.branch({ "--set-upstream-to": "origin/main" });
 			let statusUponOpening = await git.fetch().status();
-			if (statusUponOpening.behind > 0)
-			{
+			if (statusUponOpening.behind > 0) {
 				// Automatically sync if needed
-				if (this.settings.isSyncOnLoad == true)
-				{
+				if (this.settings.isSyncOnLoad == true) {
 					this.SyncNotes();
+				} else {
+					new Notice(
+						"GitHub Sync: " +
+							statusUponOpening.behind +
+							" commits behind remote.\nClick the GitHub ribbon icon to sync."
+					);
 				}
-				else
-				{
-					new Notice("GitHub Sync: " + statusUponOpening.behind + " commits behind remote.\nClick the GitHub ribbon icon to sync.")
-				}
-			}
-			else
-			{
-				new Notice("GitHub Sync: up to date with remote.")
+			} else {
+				new Notice("GitHub Sync: up to date with remote.");
 			}
 		} catch (e) {
 			// don't care
@@ -199,12 +236,14 @@ export default class GHSyncPlugin extends Plugin {
 		}
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -221,61 +260,86 @@ class GHSyncSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		const howto = containerEl.createEl("div", { cls: "howto" });
-		howto.createEl("div", { text: "How to use this plugin", cls: "howto_title" });
-		howto.createEl("small", { text: "Grab your GitHub repository's HTTPS or SSH url and paste it into the settings here. If you're not authenticated, the first sync with this plugin should prompt you to authenticate. If you've already setup SSH on your device with GitHub, you won't need to authenticate - just paste your repo's SSH url into the settings here.", cls: "howto_text" });
+		howto.createEl("div", {
+			text: "How to use this plugin",
+			cls: "howto_title",
+		});
+		howto.createEl("small", {
+			text: "Grab your GitHub repository's HTTPS or SSH url and paste it into the settings here. If you're not authenticated, the first sync with this plugin should prompt you to authenticate. If you've already setup SSH on your device with GitHub, you won't need to authenticate - just paste your repo's SSH url into the settings here.",
+			cls: "howto_text",
+		});
 		howto.createEl("br");
-        const linkEl = howto.createEl('p');
-        linkEl.createEl('span', { text: 'See the ' });
-        linkEl.createEl('a', { href: 'https://github.com/kevinmkchin/Obsidian-GitHub-Sync/blob/main/README.md', text: 'README' });
-        linkEl.createEl('span', { text: ' for more information and troubleshooting.' });
+		const linkEl = howto.createEl("p");
+		linkEl.createEl("span", { text: "See the " });
+		linkEl.createEl("a", {
+			href: "https://github.com/kevinmkchin/Obsidian-GitHub-Sync/blob/main/README.md",
+			text: "README",
+		});
+		linkEl.createEl("span", {
+			text: " for more information and troubleshooting.",
+		});
 
 		new Setting(containerEl)
-			.setName('Remote URL')
-			.setDesc('')
-			.addText(text => text
-				.setPlaceholder('')
-				.setValue(this.plugin.settings.remoteURL)
-				.onChange(async (value) => {
-					this.plugin.settings.remoteURL = value;
-					await this.plugin.saveSettings();
-				})
-        	.inputEl.addClass('my-plugin-setting-text'));
+			.setName("Remote URL")
+			.setDesc("")
+			.addText((text) =>
+				text
+					.setPlaceholder("")
+					.setValue(this.plugin.settings.remoteURL)
+					.onChange(async (value) => {
+						this.plugin.settings.remoteURL = value;
+						await this.plugin.saveSettings();
+					})
+					.inputEl.addClass("my-plugin-setting-text")
+			);
 
 		new Setting(containerEl)
-			.setName('[OPTIONAL] git binary location')
-			.setDesc('If git is not findable via your system PATH, then provide its location here')
-			.addText(text => text
-				.setPlaceholder('')
-				.setValue(this.plugin.settings.gitLocation)
-				.onChange(async (value) => {
-					this.plugin.settings.gitLocation = value;
-					await this.plugin.saveSettings();
-				})
-        	.inputEl.addClass('my-plugin-setting-text2'));
+			.setName("[OPTIONAL] git binary location")
+			.setDesc(
+				"If git is not findable via your system PATH, then provide its location here"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("")
+					.setValue(this.plugin.settings.gitLocation)
+					.onChange(async (value) => {
+						this.plugin.settings.gitLocation = value;
+						await this.plugin.saveSettings();
+					})
+					.inputEl.addClass("my-plugin-setting-text2")
+			);
 
 		new Setting(containerEl)
-			.setName('[OPTIONAL] Auto sync on startup')
-			.setDesc('Automatically sync when you start obsidian if there are unsynced changes')
-			.addToggle((toggle) => toggle
-				.setValue(this.plugin.settings.isSyncOnLoad)
-				.onChange(async (value) => {
-					this.plugin.settings.isSyncOnLoad = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("[OPTIONAL] Auto sync on startup")
+			.setDesc(
+				"Automatically sync when you start obsidian if there are unsynced changes"
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.isSyncOnLoad)
+					.onChange(async (value) => {
+						this.plugin.settings.isSyncOnLoad = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('[OPTIONAL] Auto sync at interval')
-			.setDesc('Set a positive integer minute interval after which your vault is synced automatically. Auto sync is disabled if this field is left empty or not a positive integer. Restart Obsidan to take effect.')
-			.addText(text => text
-				.setValue(String(this.plugin.settings.syncinterval))
-				.onChange(async (value) => {
-					this.plugin.settings.syncinterval = Number(value);
-					await this.plugin.saveSettings();
-				}));
+			.setName("[OPTIONAL] Auto sync at interval")
+			.setDesc(
+				"Set a positive integer minute interval after which your vault is synced automatically. Auto sync is disabled if this field is left empty or not a positive integer. Restart Obsidan to take effect."
+			)
+			.addText((text) =>
+				text
+					.setValue(String(this.plugin.settings.syncinterval))
+					.onChange(async (value) => {
+						this.plugin.settings.syncinterval = Number(value);
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
